5aa4908466930773cfecd432b16e52fae952c56f
Theo-fra <jokeraresene@gmail.com>
2025-03-22 16:00:04 +0100

    v 0.2.22 ajout de tranform

--- Assets/Scripts/TriggerSeePlayer.cs
+++ Assets/Scripts/TriggerSeePlayer.cs
@@ -12,6 +12,7 @@ public class TriggerSeePlayer : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Guard_02Anim.SetTrigger("THello");
+            transform.Rotate(new Vector3(0f, 156f, 0f));
         }
     }
     private void OnTriggerExit(Collider other)
@@ -19,6 +20,7 @@ public class TriggerSeePlayer : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             Guard_02Anim.SetTrigger("TPunch");
+            transform.Rotate(new Vector3(0f, -156f, 0f));
         }
     }
 }

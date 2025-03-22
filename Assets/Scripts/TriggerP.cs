c713c94d47d28cde82a3c3206b999923adebc964
Theo-fra <jokeraresene@gmail.com>
2025-03-22 15:32:50 +0100

    v 0.2.21 script et suppresion

---
+++ Assets/Scripts/TriggerP.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TriggerP : MonoBehaviour
+{
+    private Animator PeasantAnim;
+    void Start()
+    {
+        PeasantAnim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PeasantAnim.SetTrigger("THello");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PeasantAnim.SetTrigger("TTask");
+        }
+    }
+}
